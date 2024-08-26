b25c002af5ecace489a7080a0502251a0bc67acf
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-08-26 01:35:43 +0100

    Add some logs

--- server.py
+++ server.py
@@ -29,18 +29,22 @@ async def generateBusinessPlan(data: InputData):
         land_id = data.land_id
 
         # Fetch necessary data from the database using the land_id and user_id
+        print('Fetching model inputs...')
         model_inputs = await get_model_inputs(land_id)
 
         # Pass the model inputs to the crop prediction function
+        print('Predicting and optimizing crops...')
         cropData = predict_optimize_crops_main(model_inputs)
         if isinstance(cropData, str):
             cropData = json.loads(cropData)
             
+        print('Generating business plan...')
         businessPlan = generate_business_plan_main(model_inputs, cropData)
         if isinstance(businessPlan, str):
             businessPlan = json.loads(businessPlan)
 
         # Save the business plan and crop data to the database
+        print('Saving data to the database...')
         await process_business_plan_and_save(businessPlan, land_id)   
         await process_crops_and_save(cropData, land_id)    
 
