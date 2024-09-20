74ab005d9958efe03d5c1bdcc1b7b82e34dcbbfb
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-09-20 15:05:56 +0100

    Prisma generation config

--- generated/landclient/client.py
+++ generated/landclient/client.py
@@ -85,10 +85,10 @@ __all__ = (
 
 log: logging.Logger = logging.getLogger(__name__)
 
-SCHEMA_PATH = Path('C:/Users/STORM Tech/Desktop/Code/A2SV/A2SV-Agrisistance-Model-Backend/prisma/schema.land.prisma')
+SCHEMA_PATH = Path('C:/Users/STORM Tech/Desktop/Code/A2SV/A2SV-agrisistance-model-service/prisma/schema.land.prisma')
 PACKAGED_SCHEMA_PATH = Path(__file__).parent.joinpath('schema.prisma')
 ENGINE_TYPE: EngineType = EngineType.binary
-BINARY_PATHS = model_parse(BinaryPaths, {'queryEngine': {'windows': 'C:\\Users\\STORM Tech\\.cache\\prisma-python\\binaries\\5.17.0\\393aa359c9ad4a4bb28630fb5613f9c281cde053\\node_modules\\prisma\\query-engine-windows.exe'}, 'introspectionEngine': {}, 'migrationEngine': {}, 'libqueryEngine': {}, 'prismaFmt': {}})
+BINARY_PATHS = model_parse(BinaryPaths, {'queryEngine': {'windows': 'C:\\Users\\STORM Tech\\.cache\\prisma-python\\binaries\\5.17.0\\393aa359c9ad4a4bb28630fb5613f9c281cde053\\node_modules\\prisma\\query-engine-windows.exe', 'linux-musl-openssl-3.0.x': 'C:\\Users\\STORM Tech\\.cache\\prisma-python\\binaries\\5.17.0\\393aa359c9ad4a4bb28630fb5613f9c281cde053\\node_modules\\prisma\\query-engine-linux-musl-openssl-3.0.x'}, 'introspectionEngine': {}, 'migrationEngine': {}, 'libqueryEngine': {}, 'prismaFmt': {}})
 
 
 class Prisma(AsyncBasePrisma):
@@ -160,7 +160,7 @@ class Prisma(AsyncBasePrisma):
         return {
             'name': 'db',
             'url': OptionalValueFromEnvVar(**{'value': None, 'fromEnvVar': 'DATABASE_LAND_URL'}).resolve(),
-            'source_file_path': 'C:/Users/STORM Tech/Desktop/Code/A2SV/A2SV-Agrisistance-Model-Backend/prisma/schema.land.prisma',
+            'source_file_path': 'C:/Users/STORM Tech/Desktop/Code/A2SV/A2SV-agrisistance-model-service/prisma/schema.land.prisma',
         }
 
     async def execute_raw(self, query: LiteralString, *args: Any) -> int:
