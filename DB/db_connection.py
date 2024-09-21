936f7801828c9ebc61dbf2dc999ac9ea48dbfed1
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2024-09-21 01:13:34 +0100

    Changes in db_connection

--- DB/db_connection.py
+++ DB/db_connection.py
@@ -2,7 +2,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 
-def get_db_connect():
+def get_db_connection():
     connection = psycopg2.connect(
         host= os.getenv('DB_HOST'),
         database= os.getenv('DB_NAME'),
@@ -11,5 +11,3 @@ def get_db_connect():
         port= 50013
     )
     return connection
-
-get_db_connection = get_db_connect()
